6c3f2aeb77678c2b1675dff051a89126da234a9d
Aman Singh <138305220+amanchauhan1999@users.noreply.github.com>
2024-11-12 22:24:45 +0530

    Add files via upload

---
+++ C Basics/add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Enter the numbers :");
+    scanf(" %d %d",&a,&b);
+    int c = a+b;
+    printf("%d",c);
+    return 0;
+}
